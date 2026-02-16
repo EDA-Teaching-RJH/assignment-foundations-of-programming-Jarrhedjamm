6ed1c4a5e0601bce3b40c63a6b97090b33b01f60
Jarrhedjamm <jm13@kent.ac.uk>
2026-02-16 09:33:07 +0000

    added the names ranks divisions and ids

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+names = ["picard", "spock", "data", "kirk", "worf"]
+ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
+divisions = ["Command", "Command", "Operations", "Security"]
+id = ["1234" , "5678", "9101112", "13141516", "17181920"
+      
