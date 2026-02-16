d3ae09acc378c99fd6e404787ef5a3e6644e57fe
Jarrhedjamm <jm13@kent.ac.uk>
2026-02-16 10:56:32 +0000

    added the choices and have made the display name and exit feature

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,5 +1,32 @@
-names = ["picard", "spock", "data", "kirk", "worf"]
-ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant"]
-divisions = ["Command", "Command", "Operations", "Security"]
-id = ["1234" , "5678", "9101112", "13141516", "17181920"
-      
+def init_database():
+    names = ["picard", "spock", "data", "kirk", "worf"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "ensign"]
+    divisions = ["Command", "Command", "Operations", "Security", "research"]
+    id = ["1234" , "5678", "9101112", "13141516", "17181920"]
+
+    return names, ranks, divisions, id
+
+def display_menu(Full_Name):
+        print("fleet manager menu")
+        print(f"currently logged in: {Full_Name}")
+        print("1.display roster")
+        print("2. add member")
+        print("3.remove member")
+        print("4.update rank")
+        print("5.search crew")
+        print("6.filter by division")
+        print("7.calculate payroll")
+        print("8.count officers")
+        print("9.exit")
+        choice = input ("select option:")
+        return choice
+
+def main():
+      n, r, d, i = init_database()
+      user = input("provide your Full Name")
+      while True:
+            choice =display_menu(user)
+
+            if choice == "9":
+                print("Goodbye, captain")
+            break
